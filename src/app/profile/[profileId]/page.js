f0697f8ecb3ae1fd5b0b4b0fad258278d0026474
Mahpara <mah4.para@gmail.com>
2025-10-16 09:57:47 +0100

    chore: added jwt tokens and decoded tokens for demo purposes

--- src/app/profile/[profileId]/page.js
+++ src/app/profile/[profileId]/page.js
@@ -15,7 +15,23 @@ export default async function ProfilePage({ params: { profileId } }) {
     return (
     <main className="p-6">
         <h1>Welcome, {user.username}!</h1>
-        <p>This is your profile page with ID: {profileId}</p>
+        <p>This is your super secret profile page with ID: {profileId}</p>
+         {/* ✅ Displayed the JWT */}
+        <section className="bg-gray-100 p-4 rounded">
+        <h2 className="text-lg font-semibold">JWT Token:</h2>
+        <pre className="break-words text-sm bg-white p-2 rounded border">
+            {token}
+        </pre>
+        </section>
+
+      {/* ✅ Displayed decoded payload */}
+        <section className="bg-gray-100 p-4 rounded">
+        <h2 className="text-lg font-semibold">Decoded Token:</h2>
+        <pre className="text-sm bg-white p-2 rounded border">
+            {/* JavaScript function that converts a JavaScript object into a readable JSON string, JSON.stringify(value, replacer, space) */}
+            {JSON.stringify(user, null, 2)}
+        </pre>
+        </section>
         <form action={logoutAction} className="mt-6">
         <button
             type="submit"
